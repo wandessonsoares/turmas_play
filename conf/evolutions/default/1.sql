4f11f7ea3ab836bebbf13d1a7e2be6e1698b80bf
wandessonsoares <wandessonsilvatsi@gmail.com>
2014-08-07 10:14:44 -0300

    conectando ao BD em memória

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,27 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table turma (
+  id                        integer not null,
+  nome                      varchar(255),
+  periodo                   varchar(255),
+  constraint pk_turma primary key (id))
+;
+
+create sequence turma_seq;
+
+
+
+
+# --- !Downs
+
+SET REFERENTIAL_INTEGRITY FALSE;
+
+drop table if exists turma;
+
+SET REFERENTIAL_INTEGRITY TRUE;
+
+drop sequence if exists turma_seq;
+
