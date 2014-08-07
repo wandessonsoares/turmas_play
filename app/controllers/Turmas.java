9c74958e12ed44464acec31ce50a3db27a20f36d
wandessonsoares <wandessonsilvatsi@gmail.com>
2014-08-06 22:25:00 -0300

    Adição da view cadastrarTurma, configuração da rota, e correção dos links no template

--- app/controllers/Turmas.java
+++ app/controllers/Turmas.java
@@ -8,4 +8,8 @@ public class Turmas extends Controller{
 	public static Result home(){
 		return ok(views.html.home.render());
 	}
+	
+	public static Result novaTurma(){
+		return ok(views.html.cadastrarTurma.render());
+	}
 }
