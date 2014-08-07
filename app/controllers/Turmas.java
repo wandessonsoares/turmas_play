77cf4bc52104eb6a032c9da319c40d979ce6bf39
wandessonsoares <wandessonsilvatsi@gmail.com>
2014-08-07 17:03:10 -0300

    Adicionando a funcionalidade de listar turmas

--- app/controllers/Turmas.java
+++ app/controllers/Turmas.java
@@ -1,11 +1,13 @@
 package controllers;
 
-import com.avaje.ebean.Ebean;
+import java.util.List;
 
 import models.Turma;
 import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
+
+import com.avaje.ebean.Ebean;
 
 public class Turmas extends Controller{
 	private static Form<Turma> turmaForm = Form.form(Turma.class);
@@ -23,6 +25,11 @@ public class Turmas extends Controller{
 		Turma turma = formFromRequest.get();
 		Ebean.save(turma);		
 		return ok(views.html.cadastrarTurma.render("Turma salva com sucesso!"));
+	}
+	
+	public static Result listar(){
+		List<Turma> turmas = Ebean.find(Turma.class).findList();
+		return ok(views.html.turmas.render(turmas));
 	}
 	
 }
